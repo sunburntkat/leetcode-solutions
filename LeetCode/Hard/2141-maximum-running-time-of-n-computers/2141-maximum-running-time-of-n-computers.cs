96dfd8edc5a98969768433179207193715b32cd8
sunburntkat <kathirwork13@gmail.com>
2025-12-01 18:16:53 +0530

    2141-maximum-running-time-of-n-computers Hard 12-01-2025

---
+++ LeetCode/Hard/2141-maximum-running-time-of-n-computers/2141-maximum-running-time-of-n-computers.cs
@@ -0,0 +1,24 @@
+public class Solution {
+    public long MaxRunTime(int n, int[] batteries) {
+        long sum=0;
+        for(int i=0;i<batteries.Length;i++){
+            sum+=batteries[i];
+        }
+        long l=1;
+        long r=sum/n;
+        while(l<r){
+            long mid=l+(r-l)/2+1;
+            long runTime=0;
+            for(int i=0;i<batteries.Length;i++){
+                runTime+=Math.Min(batteries[i],mid);
+            }
+            if(runTime/n>=mid){
+                l=mid;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        return l;
+    }
+}
