39bcbb56722f480e88923ca98e35b785e73b6801
sunburntkat <kathirwork13@gmail.com>
2025-12-03 15:45:05 +0530

    0542-01-matrix Medium 12-03-2025

---
+++ LeetCode/Medium/0542-01-matrix/0542-01-matrix.cs
@@ -0,0 +1,38 @@
+public class Solution {
+    public int[][] UpdateMatrix(int[][] mat) {
+        Queue<(int i,int j)> queue=new();
+        HashSet<(int,int)> visited=new();
+        int[] di={0,0,1,-1};
+        int[] dj={1,-1,0,0};
+
+        for(int i=0;i<mat.Length;i++){
+            for(int j=0;j<mat[0].Length;j++){
+                if(mat[i][j]==0){
+                    queue.Enqueue((i,j));
+                }
+            }
+        }
+
+
+        int iter=0;
+        while(queue.Count!=0){
+            int n=queue.Count;
+            for(int i=0;i<n;i++){
+                var node=queue.Dequeue();
+                if(visited.Contains(node)) continue;
+                mat[node.i][node.j]=iter;
+                visited.Add(node);
+                for(int j=0;j<4;j++){
+                    int ni=node.i+di[j];
+                    int nj=node.j+dj[j];
+                    if(ni<0||nj<0||ni>=mat.Length||nj>=mat[0].Length) continue;
+                    if(mat[ni][nj]==0) continue;
+                    if(visited.Contains((ni,nj))) continue;
+                    queue.Enqueue((ni,nj));
+                }
+            }
+            iter++;
+        }
+        return mat;
+    }
+}
