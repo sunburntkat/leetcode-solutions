881f57d49693e19902bb4cde21f67163c5875a9c
sunburntkat <kathirwork13@gmail.com>
2025-12-03 14:47:08 +0530

    0733-flood-fill Easy 12-03-2025

---
+++ LeetCode/Easy/0733-flood-fill/0733-flood-fill.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[][] FloodFill(int[][] image, int sr, int sc, int color) {
+        Queue<(int i,int j)> queue=new();
+        (int i,int j)[] neighbours=new (int i,int j)[]{(0,1),(1,0),(-1,0),(0,-1)};
+        int original=image[sr][sc];
+        queue.Enqueue((sr,sc));
+        while(queue.Count!=0){
+            var node=queue.Dequeue();
+            image[node.i][node.j]=color;
+            for(int k=0;k<4;k++){
+                var neighbour=(node.i+neighbours[k].i,node.j+neighbours[k].j);
+                if(neighbour.Item1>=0&&neighbour.Item2>=0&&neighbour.Item1<image.Length&&neighbour.Item2<image[0].Length&&image[neighbour.Item1][neighbour.Item2]==original&&image[neighbour.Item1][neighbour.Item2]!=color){
+                    queue.Enqueue(neighbour);
+                }
+            }
+        }
+        return image;
+    }
+
+}
