073499f445dcfd2608698638d0031f7032c6efdc
sunburntkat <kathirwork13@gmail.com>
2025-12-03 18:02:40 +0530

    0542-01-matrix Medium 12-03-2025

--- LeetCode/Medium/0542-01-matrix/0542-01-matrix.cs
+++ LeetCode/Medium/0542-01-matrix/0542-01-matrix.cs
@@ -9,29 +9,26 @@ public class Solution {
             for(int j=0;j<mat[0].Length;j++){
                 if(mat[i][j]==0){
                     queue.Enqueue((i,j));
+                    visited[i,j]=true;
                 }
             }
         }
 
 
-        int iter=0;
         while(queue.Count!=0){
-            int n=queue.Count;
-            for(int i=0;i<n;i++){
-                var node=queue.Dequeue();
-                // if(visited.Contains(node)) continue;
-                mat[node.i][node.j]=iter;
-                for(int j=0;j<4;j++){
-                    int ni=node.i+di[j];
-                    int nj=node.j+dj[j];
-                    if(ni<0||nj<0||ni>=mat.Length||nj>=mat[0].Length) continue;
-                    if(mat[ni][nj]==0) continue;
-                    if(visited[ni,nj]) continue;
-                    queue.Enqueue((ni,nj));
-                    visited[ni,nj]=true;
-                }
+            var node=queue.Dequeue();
+            for(int j=0;j<4;j++){
+                int ni=node.i+di[j];
+                int nj=node.j+dj[j];
+
+                if(ni<0||nj<0||ni>=mat.Length||nj>=mat[0].Length) continue;
+                if(visited[ni,nj]) continue;
+                
+                queue.Enqueue((ni,nj));
+                visited[ni,nj]=true;
+                mat[ni][nj]=mat[node.i][node.j]+1;
             }
-            iter++;
+            
         }
         return mat;
     }
