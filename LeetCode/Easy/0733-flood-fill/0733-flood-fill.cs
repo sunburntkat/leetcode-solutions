1078d473f9135e55ab4e6b95ec615ca5cb255edf
sunburntkat <kathirwork13@gmail.com>
2025-12-03 14:57:48 +0530

    0733-flood-fill Easy 12-03-2025

--- LeetCode/Easy/0733-flood-fill/0733-flood-fill.cs
+++ LeetCode/Easy/0733-flood-fill/0733-flood-fill.cs
@@ -3,17 +3,17 @@ public class Solution {
         if(image[sr][sc]==color){
             return image;
         }
-        Queue<(int i,int j)> queue=new();
+        Stack<(int i,int j)> stack=new();
         (int i,int j)[] neighbours=new (int i,int j)[]{(0,1),(1,0),(-1,0),(0,-1)};
         int original=image[sr][sc];
-        queue.Enqueue((sr,sc));
-        while(queue.Count!=0){
-            var node=queue.Dequeue();
+        stack.Push((sr,sc));
+        while(stack.Count!=0){
+            var node=stack.Pop();
             image[node.i][node.j]=color;
             for(int k=0;k<4;k++){
                 var neighbour=(node.i+neighbours[k].i,node.j+neighbours[k].j);
                 if(neighbour.Item1>=0&&neighbour.Item2>=0&&neighbour.Item1<image.Length&&neighbour.Item2<image[0].Length&&image[neighbour.Item1][neighbour.Item2]==original){
-                    queue.Enqueue(neighbour);
+                    stack.Push(neighbour);
                 }
             }
         }
