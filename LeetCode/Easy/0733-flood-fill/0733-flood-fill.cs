7e2c961c64455efbd7385b0041808cb3e4bffe4e
sunburntkat <kathirwork13@gmail.com>
2025-12-03 14:55:27 +0530

    0733-flood-fill Easy 12-03-2025

--- LeetCode/Easy/0733-flood-fill/0733-flood-fill.cs
+++ LeetCode/Easy/0733-flood-fill/0733-flood-fill.cs
@@ -1,5 +1,8 @@
 public class Solution {
     public int[][] FloodFill(int[][] image, int sr, int sc, int color) {
+        if(image[sr][sc]==color){
+            return image;
+        }
         Queue<(int i,int j)> queue=new();
         (int i,int j)[] neighbours=new (int i,int j)[]{(0,1),(1,0),(-1,0),(0,-1)};
         int original=image[sr][sc];
@@ -9,7 +12,7 @@ public class Solution {
             image[node.i][node.j]=color;
             for(int k=0;k<4;k++){
                 var neighbour=(node.i+neighbours[k].i,node.j+neighbours[k].j);
-                if(neighbour.Item1>=0&&neighbour.Item2>=0&&neighbour.Item1<image.Length&&neighbour.Item2<image[0].Length&&image[neighbour.Item1][neighbour.Item2]==original&&image[neighbour.Item1][neighbour.Item2]!=color){
+                if(neighbour.Item1>=0&&neighbour.Item2>=0&&neighbour.Item1<image.Length&&neighbour.Item2<image[0].Length&&image[neighbour.Item1][neighbour.Item2]==original){
                     queue.Enqueue(neighbour);
                 }
             }
