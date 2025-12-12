244beb0fa4b86fd422d507a07f7f6f9768013744
sunburntkat <kathirwork13@gmail.com>
2025-12-12 13:23:45 +0530

    3433-count-mentions-per-user Medium 12-12-2025

---
+++ LeetCode/Medium/3433-count-mentions-per-user/3433-count-mentions-per-user.cs
@@ -0,0 +1,71 @@
+public class Solution {
+    public int[] CountMentions(int numberOfUsers, IList<IList<string>> events) {
+        int[] status=new int[numberOfUsers];
+        int[] mentions=new int[numberOfUsers];
+        var eventsList = events
+                .Select(e => e.ToList())
+                .ToList();
+        eventsList.Sort((a,b)=>{
+            int cmp=(Convert.ToInt32(a[1])).CompareTo(Convert.ToInt32(b[1]));
+            if(cmp==0){
+                return b[0].CompareTo(a[0]);  //"OFFLINE">"MESSAGE", we swapped the parameters around (b.compareTo(a))
+            }
+            return cmp;
+        });
+
+        int lastModifiedTime=0;
+        for(int i=0;i<eventsList.Count;i++){
+            var curEvent=eventsList[i];
+            if(curEvent[0]=="OFFLINE"){
+                int currentTime=Convert.ToInt32(curEvent[1]);
+                int timePassed=currentTime-lastModifiedTime;
+                for(int j=0;j<status.Length;j++){
+                    int id=Convert.ToInt32(j);
+                    if(status[id]>0){
+                        if(timePassed>=status[id]){
+                            status[id]=0;
+                        }
+                        else{
+                            status[id]-=timePassed;
+                        }
+                    }
+                }
+                status[Convert.ToInt32(curEvent[2])]=60;
+                lastModifiedTime=currentTime;
+            }
+            else{
+                if(curEvent[2]=="ALL"){
+                    for(int j=0;j<mentions.Length;j++){
+                        mentions[j]++;
+                    }
+                }
+                else if(curEvent[2]=="HERE"){
+                    int timePassed=Convert.ToInt32(curEvent[1])-lastModifiedTime;
+                    for(int j=0;j<status.Length;j++){
+                        int id=Convert.ToInt32(j);
+                        if(status[id]>0){
+                            if( timePassed>=status[id]){
+                                status[id]=0;
+                            }
+                            else{
+                                status[id]-=timePassed;
+                            }
+                        }
+                        if(status[id]==0){
+                            mentions[id]++;
+                        }
+                    }
+                    lastModifiedTime=Convert.ToInt32(curEvent[1]);
+                }
+                else{
+                    var ids=curEvent[2].Split(' ');
+                    for(int j=0;j<ids.Length;j++){
+                        int id=Convert.ToInt32((ids[j].Substring(2,ids[j].Length-2)));
+                        mentions[id]++;
+                    }
+                }
+            }
+        }
+        return mentions;
+    }
+}
