c6f2dfa70be6d6500540354b3a764ccc692dc0b0
sunburntkat <kathirwork13@gmail.com>
2026-04-21 14:43:43 +0530

    0023-merge-k-sorted-lists Hard 04-21-2026

---
+++ LeetCode/Hard/0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.java
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq=new PriorityQueue<>((a,b)->Integer.compare(a.val,b.val));
+        ListNode result=new ListNode(-1);
+        ListNode curr=result;
+        for (int i=0;i<lists.length;i++){
+            if(lists[i]==null) continue;
+            pq.add(lists[i]);
+        }
+        while(!pq.isEmpty()){
+            ListNode min=pq.poll();
+            if(min.next!=null){
+                pq.add(min.next);
+            }
+            curr.next=new ListNode(min.val);
+            curr=curr.next;   
+        }
+        return result.next;
+    }
+}
