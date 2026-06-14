5dc478fbd0e6a1650ab3d2d44b7a0cc504b87082
sunburntkat <kathirwork13@gmail.com>
2026-06-14 13:18:12 +0530

    0103-binary-tree-zigzag-level-order-traversal Medium 06-14-2026

--- LeetCode/Medium/0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
+++ LeetCode/Medium/0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.java
@@ -14,53 +14,31 @@
  * }
  */
 class Solution {
-    List<List<Integer>> result=new ArrayList<>();
+    List<List<Integer>> result = new ArrayList<>();
+
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        Stack<TreeNode> stack=new Stack<>();
-        stack.push(root);
-        recForward(stack);
+        Stack<TreeNode> prevStack = new Stack<>();
+        prevStack.push(root);
+        int i=0;
+        while (!prevStack.isEmpty()) {
+            List<Integer> levelInt = new ArrayList<>();
+            Stack<TreeNode> stack = new Stack<>();
+            while (!prevStack.isEmpty()) {
+                TreeNode node = prevStack.pop();
+                if (node == null)
+                    continue;
+                levelInt.add(node.val);
+                stack.push(i%2==0?node.left:node.right);
+                stack.push(i%2==0?node.right:node.left);
+            }
+            if (levelInt.isEmpty()) {
+                continue;
+            }
+            result.add(levelInt);
+            prevStack=stack;
+            i++;
+            
+        }
         return result;
-    }
-
-    public void recForward(Stack<TreeNode> prevStack){
-        if(prevStack.isEmpty()){
-            return;
-        }
-        
-        List<Integer> levelInt=new ArrayList<>();
-        Stack<TreeNode> stack=new Stack<>();
-        while(!prevStack.isEmpty()){
-            TreeNode node=prevStack.pop();
-            if(node==null) continue;
-            levelInt.add(node.val);
-            stack.push(node.left);
-            stack.push(node.right);
-        }
-        if(levelInt.isEmpty()){
-            return;
-        }
-        result.add(levelInt);
-        recReverse(stack);
-    }
-    public void recReverse(Stack<TreeNode> prevStack){
-        if(prevStack.isEmpty()){
-            return;
-        }
-        
-        List<Integer> levelInt=new ArrayList<>();
-        Stack<TreeNode> stack=new Stack<>();
-        while(!prevStack.isEmpty()){
-            TreeNode node=prevStack.pop();
-            if(node==null) continue;
-            levelInt.add(node.val);
-            stack.push(node.right);
-            stack.push(node.left);
-        }
-        if(levelInt.isEmpty()){
-            return;
-        }
-        result.add(levelInt);
-        recForward(stack);
-
     }
 }
