488b377fe637fc7e4bcc32507ad8369dba180fbf
sunburntkat <kathirwork13@gmail.com>
2026-06-14 12:31:07 +0530

    0100-same-tree Easy 06-14-2026

--- LeetCode/Easy/0100-same-tree/0100-same-tree.java
+++ LeetCode/Easy/0100-same-tree/0100-same-tree.java
@@ -15,10 +15,10 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q!=null || q==null && p!=null){
+        if(p==null && q==null) return true;
+        if(p==null || q==null ){
             return false;
         }
-        if(p==null && q==null) return true;
         if(p.val!=q.val){
             return false;
         }
