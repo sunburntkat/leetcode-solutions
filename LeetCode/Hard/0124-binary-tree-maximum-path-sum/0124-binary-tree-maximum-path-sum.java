2bc18d0974975b01fc5b56f9ce8f2b8c08d2af92
sunburntkat <kathirwork13@gmail.com>
2026-06-14 12:14:37 +0530

    0124-binary-tree-maximum-path-sum Hard 06-14-2026

--- LeetCode/Hard/0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
+++ LeetCode/Hard/0124-binary-tree-maximum-path-sum/0124-binary-tree-maximum-path-sum.java
@@ -14,33 +14,24 @@
  * }
  */
 class Solution {
-    int maxSum=Integer.MIN_VALUE;
+    int maxSum = Integer.MIN_VALUE;
+
     public int maxPathSum(TreeNode root) {
         getMaxHeightSum(root);
         return maxSum;
     }
 
-    public Integer getMaxHeightSum(TreeNode root){
-        if(root==null) return null;
-        Integer leftMax=getMaxHeightSum(root.left);
-        Integer rightMax=getMaxHeightSum(root.right);
-        
-        int maxHeightSum= Math.max(leftMax==null?Integer.MIN_VALUE:leftMax,rightMax==null?Integer.MIN_VALUE:rightMax);
-        if(maxHeightSum<0){
-            maxSum=Math.max(maxSum,root.val);
-            return root.val;
+    public int getMaxHeightSum(TreeNode root) {
+        if (root == null)
+            return 0;
+        int leftMax = Math.max(0, getMaxHeightSum(root.left));
+        int rightMax = Math.max(0, getMaxHeightSum(root.right));
+
+        int maxHeightSum = Math.max(leftMax, rightMax);
+
+        if (leftMax + rightMax + root.val > maxSum) {
+            maxSum = leftMax + rightMax + root.val;
         }
-        else{
-        if((leftMax==null?0:leftMax)+(rightMax==null?0:rightMax)+root.val>maxSum){
-            maxSum=(leftMax==null?0:leftMax)+(rightMax==null?0:rightMax)+root.val;
-        }
-        if((leftMax==null?0:leftMax)+root.val>maxSum){
-            maxSum=(leftMax==null?0:leftMax)+root.val;
-        }
-        if((rightMax==null?0:rightMax)+root.val>maxSum){
-            maxSum=(rightMax==null?0:rightMax)+root.val;
-        }
-            return root.val+maxHeightSum;
-        }
+        return root.val + maxHeightSum;
     }
 }
