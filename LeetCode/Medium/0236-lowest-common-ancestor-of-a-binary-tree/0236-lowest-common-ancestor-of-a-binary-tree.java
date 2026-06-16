10b53b35da57764305eeaa651b4c86bf75df557f
sunburntkat <kathirwork13@gmail.com>
2026-06-16 22:20:46 +0530

    0236-lowest-common-ancestor-of-a-binary-tree Medium 06-16-2026

--- LeetCode/Medium/0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ LeetCode/Medium/0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -7,43 +7,28 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
+
 class Solution {
+    TreeNode parent = null;
+
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        TreeNode curr = root;
-        TreeNode parent = null;
-        TreeNode prev = null;
-        Stack<TreeNode> stack = new Stack<>();
-        boolean found1=false;
+        recLowestCommonAncestor(root,p,q);
+        return parent;
+    }
 
-        while (curr != null || !stack.isEmpty()) {
-            while(curr!=null){
-                stack.push(curr);
-                curr=curr.left;
-
-            }
-            curr=stack.peek();
-            if(curr.right==null || curr.right==prev){
-                if(curr.val==p.val || curr.val==q.val){
-                    if(!found1){
-                        found1=true;
-                        parent=curr;
-                    } 
-                    else{
-                        return parent;
-                    }
-                }
-                stack.pop();
-                if(curr==parent){
-                    parent=stack.peek();
-                }
-                prev=curr;
-                curr=null;
-            }
-            else{
-                curr=curr.right;
-            }
+    public boolean recLowestCommonAncestor(TreeNode node, TreeNode p, TreeNode q) {
+        if (node == null){
+            return false;
         }
-        return null;
-
+        if (node == p || node == q) {
+            if(parent==null) parent = node;
+            return true;
+        }
+        boolean left = recLowestCommonAncestor(node.left, p, q);
+        boolean right = recLowestCommonAncestor(node.right, p, q);
+        if(left && right){
+            parent=node;
+        }
+        return left || right;
     }
 }
