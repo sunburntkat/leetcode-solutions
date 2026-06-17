26e68d927629f7d758e2587b54863c58802833e6
sunburntkat <kathirwork13@gmail.com>
2026-06-17 14:09:27 +0530

    0662-maximum-width-of-binary-tree Medium 06-17-2026

--- LeetCode/Medium/0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
+++ LeetCode/Medium/0662-maximum-width-of-binary-tree/0662-maximum-width-of-binary-tree.java
@@ -25,21 +25,18 @@ class Solution {
             List<NodePoint> newLevel = new ArrayList<>();
             for (int i = 0; i < level.size(); i++) {
                 NodePoint nodePoint = level.get(i);
-                if (nodePoint.node() == null)
-                    continue;
-                newLevel.add(new NodePoint(nodePoint.node().left, nodePoint.x() * 2 - 1));
-                newLevel.add(new NodePoint(nodePoint.node().right, nodePoint.x() * 2));
+                if (nodePoint.node().left != null) {
+                    newLevel.add(new NodePoint(nodePoint.node().left, nodePoint.x() * 2 - 1));
+                }
+                if (nodePoint.node().right != null) {
+                    newLevel.add(new NodePoint(nodePoint.node().right, nodePoint.x() * 2));
+                }
             }
-            int l=0;
-            int r=newLevel.size()-1;
-            while(l<=r && newLevel.get(l).node()==null){
-                l++;
-            }
-            while(l<=r && newLevel.get(r).node()==null){
-                r--;
-            }
-            if (l<=r) {
-                int width = newLevel.get(r).x() - newLevel.get(l).x()+1;
+            int l = 0;
+            int r = newLevel.size() - 1;
+            
+            if (l <= r) {
+                int width = newLevel.get(r).x() - newLevel.get(l).x() + 1;
                 if (width > maxWidth) {
                     maxWidth = width;
                 }
