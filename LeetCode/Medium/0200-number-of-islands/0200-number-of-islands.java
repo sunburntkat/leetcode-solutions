07be581f13c4816f87d40be5666e3711b181cdfe
sunburntkat <kathirwork13@gmail.com>
2026-06-25 17:33:03 +0530

    0200-number-of-islands Medium 06-25-2026

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
@@ -10,17 +10,21 @@ class Solution {
             for(int j=0;j<n;j++){
                 if(visited[i][j]||grid[i][j]=='0') continue;
                 q.offer(new int[]{i,j});
+
+                int[] dx={-1,1,0,0};
+                int[] dy={0,0,1,-1};
                 while(!q.isEmpty()){
                     int[] c=q.poll();
-                    if(c[0]<0 || c[0]>=m) continue;
-                    if(c[1]<0 || c[1]>=n) continue;
-                    if(visited[c[0]][c[1]]) continue;
-                    if(grid[c[0]][c[1]]=='0') continue;
-                    q.offer(new int[]{c[0]+1,c[1]});
-                    q.offer(new int[]{c[0]-1,c[1]});
-                    q.offer(new int[]{c[0],c[1]+1});
-                    q.offer(new int[]{c[0],c[1]-1});
-                    visited[c[0]][c[1]]=true;
+                    for(int k=0;k<4;k++){
+                        int nr=c[0]+dy[k];
+                        int nc=c[1]+dx[k];
+                        if(nr<0 || nr>=m) continue;
+                        if(nc<0 || nc>=n) continue;
+                        if(visited[nr][nc]) continue;
+                        if(grid[nr][nc]=='0') continue;
+                        q.offer(new int[]{nr,nc});
+                        visited[nr][nc]=true;
+                    }
                 }
                 count++;
             }
