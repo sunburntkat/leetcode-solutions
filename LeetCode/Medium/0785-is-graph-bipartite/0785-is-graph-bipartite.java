c1857e892e7b2a6927b6b6d15fca3d330823250a
sunburntkat <kathirwork13@gmail.com>
2026-06-25 22:29:20 +0530

    0785-is-graph-bipartite Medium 06-25-2026

--- LeetCode/Medium/0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ LeetCode/Medium/0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,29 +1,25 @@
 class Solution {
     public boolean isBipartite(int[][] graph) {
-        boolean isSetA=true;
         Queue<Integer> q =new LinkedList<>();
-        HashSet<Integer> setA=new HashSet<>();
-        HashSet<Integer> setB=new HashSet<>();
-        HashSet<Integer> visited=new HashSet<>();
+        int[] colours=new int[graph.length];
         for(int k=0;k<graph.length;k++){
-            if(visited.contains(k)) continue;
+            if(colours[k]!=0) continue;
+            colours[k]=1;
             q.offer(k);
-            visited.add(k);
             while(!q.isEmpty()){
                 int size=q.size();
                 for(int i=0;i<size;i++){
                     int node=q.poll();
-                    HashSet<Integer> set=isSetA? setA:setB;
-                    HashSet<Integer> otherSet=isSetA? setB:setA;
-                    visited.add(node);
-                    if(otherSet.contains(node)) return false;
-                    set.add(node);
                     for(int j=0;j<graph[node].length;j++){
-                        if(visited.contains(graph[node][j])) continue;
-                        q.offer(graph[node][j]);
+                        int neighbour=graph[node][j];
+                        if(colours[neighbour]==0){
+                            colours[neighbour]=-colours[node];
+                            q.offer(neighbour);
+                        }else if(colours[neighbour]==colours[node]){
+                            return false;
+                        }
                     }
                 }
-                isSetA=!isSetA;
             }
         }
         return true;
