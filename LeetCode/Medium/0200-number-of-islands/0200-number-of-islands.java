5c60687d2a25ebe8a791ea748d8fc5ac005983db
sunburntkat <kathirwork13@gmail.com>
2026-06-25 17:36:02 +0530

    0200-number-of-islands Medium 06-25-2026

--- LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
+++ LeetCode/Medium/0200-number-of-islands/0200-number-of-islands.java
@@ -1,30 +1,27 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        //im gonna do it without modifying grid, hence the extra matrix
         int m=grid.length;
         int n=grid[0].length;
-        boolean[][] visited=new boolean[m][n];
         int count=0;
         Queue<int[]> q=new LinkedList<>();
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(visited[i][j]||grid[i][j]=='0') continue;
+                if(grid[i][j]=='0') continue;
                 q.offer(new int[]{i,j});
 
                 int[] dx={-1,1,0,0};
                 int[] dy={0,0,1,-1};
                 while(!q.isEmpty()){
                     int[] c=q.poll();
-                    visited[c[0]][c[1]]=true;
+                    grid[c[0]][c[1]]='0';
                     for(int k=0;k<4;k++){
                         int nr=c[0]+dy[k];
                         int nc=c[1]+dx[k];
                         if(nr<0 || nr>=m) continue;
                         if(nc<0 || nc>=n) continue;
-                        if(visited[nr][nc]) continue;
                         if(grid[nr][nc]=='0') continue;
                         q.offer(new int[]{nr,nc});
-                        visited[nr][nc]=true;
+                        grid[nr][nc]='0';
                     }
                 }
                 count++;
