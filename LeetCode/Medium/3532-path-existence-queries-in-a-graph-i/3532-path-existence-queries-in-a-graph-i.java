fef74eb29bb1d78e5e6cf6e417ebff578f52d857
sunburntkat <kathirwork13@gmail.com>
2026-07-11 19:47:50 +0530

    3532-path-existence-queries-in-a-graph-i Medium 07-11-2026

--- LeetCode/Medium/3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.java
+++ LeetCode/Medium/3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.java
@@ -1,58 +1,18 @@
 class Solution {
     public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
-        DSU ds = new DSU(n);
         Arrays.sort(nums);
-        int l = 0;
-        for (int r = 1; r < n; r++) {
-            if (Math.abs(nums[r] - nums[l]) <= maxDiff) {
-                ds.union(l, r);
-            } else if(l<r-1){
-                r--;
+        int[] groups = new int[n];
+        int groupNo = 0;
+        for (int i = 1; i < n; i++) {
+            if (Math.abs(nums[i] - nums[i - 1]) > maxDiff) {
+                groupNo++;
             }
-            l++;
+            groups[i] = groupNo;
         }
         boolean[] result = new boolean[queries.length];
         for (int i = 0; i < result.length; i++) {
-            result[i] = ds.findParent(queries[i][0]) == ds.findParent(queries[i][1]);
+            result[i] = groups[queries[i][0]] == groups[queries[i][1]];
         }
         return result;
     }
-}
-
-class DSU {
-    int[] parents;
-    int[] sizes;
-
-    public DSU(int n) {
-        parents = new int[n];
-        sizes = new int[n];
-        for (int i = 0; i < n; i++) {
-            parents[i] = i;
-            sizes[i] = 1;
-        }
-    }
-
-    public int findParent(int node) {
-        if (parents[node] == node) {
-            return node;
-        }
-
-        return parents[node] = findParent(parents[node]);
-    }
-
-    public void union(int n1, int n2) {
-        int p1 = findParent(n1);
-        int p2 = findParent(n2);
-
-        if (p1 == p2)
-            return;
-        if (sizes[p1] > sizes[p2]) {
-            parents[p2] = p1;
-            sizes[p1] += sizes[p2];
-        } else {
-            parents[p1] = p2;
-            sizes[p2] += sizes[p1];
-        }
-    }
-
 }
