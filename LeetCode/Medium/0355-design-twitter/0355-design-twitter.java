02bbe8056aea1de3a1d9b131b7d8104789bcf990
sunburntkat <kathirwork13@gmail.com>
2026-07-13 15:44:13 +0530

    0355-design-twitter Medium 07-13-2026

--- LeetCode/Medium/0355-design-twitter/0355-design-twitter.java
+++ LeetCode/Medium/0355-design-twitter/0355-design-twitter.java
@@ -1,6 +1,6 @@
 class Twitter {
 
-    HashMap<Integer, HashMap<Integer, Boolean>> following = new HashMap<>();
+    HashMap<Integer, HashSet<Integer>> following = new HashMap<>();
     int time = 0;
     HashMap<Integer, List<int[]>> tweets = new HashMap<>();
 
@@ -17,34 +17,36 @@ class Twitter {
 
     public List<Integer> getNewsFeed(int userId) {
         List<Integer> result = new ArrayList<>();
-        PriorityQueue<int[]> feed = new PriorityQueue<>((a, b) -> Integer.compare(b[0], a[0]));
-        if (!(following.get(userId) == null)) {
-            for (int followee : following.get(userId).keySet()) {
+        PriorityQueue<TweetPost> feed = new PriorityQueue<>((a, b) -> Integer.compare(b.time, a.time));
+        if (following.get(userId) != null) {
+            for (int followee : following.get(userId)) {
                 if (tweets.get(followee) == null)
                     continue;
-                int tweetCount = Math.min(tweets.get(followee).size(), 10);
-                for (int i = tweets.get(followee).size() - 1; i >= tweets.get(followee).size() - tweetCount; i--) {
-                    feed.add(tweets.get(followee).get(i));
-                }
+                int size=tweets.get(followee).size();
+                int[] firstTweet = tweets.get(followee).get(size-1);
+                feed.add(new TweetPost(firstTweet[1], firstTweet[0], followee, size-1));
             }
         }
         if (!(tweets.get(userId) == null)) {
-            int tweetCount = Math.min(tweets.get(userId).size(), 10);
-            for (int i = tweets.get(userId).size() - 1; i >= tweets.get(userId).size() - tweetCount; i--) {
-                feed.add(tweets.get(userId).get(i));
-            }
+            int size=tweets.get(userId).size();
+            int[] firstTweet = tweets.get(userId).get(size-1);
+            feed.add(new TweetPost(firstTweet[1], firstTweet[0], userId, size-1));
         }
         for (int i = 0; i < 10 && !feed.isEmpty(); i++) {
-            result.add(feed.poll()[1]);
+            TweetPost tweetPost=feed.poll();
+            result.add(tweetPost.tweetId);
+            if(tweetPost.index<=0) continue;
+            int[] nextTweet=tweets.get(tweetPost.userId).get(--tweetPost.index);
+            feed.add(new TweetPost(nextTweet[1],nextTweet[0],tweetPost.userId, tweetPost.index));
         }
         return result;
     }
 
     public void follow(int followerId, int followeeId) {
         if (!following.containsKey(followerId)) {
-            following.put(followerId, new HashMap<>());
+            following.put(followerId, new HashSet<>());
         }
-        following.get(followerId).put(followeeId, true);
+        following.get(followerId).add(followeeId);
     }
 
     public void unfollow(int followerId, int followeeId) {
@@ -62,3 +64,17 @@ class Twitter {
  * obj.follow(followerId,followeeId);
  * obj.unfollow(followerId,followeeId);
  */
+
+class TweetPost {
+    public int tweetId;
+    public int time;
+    public int userId;
+    public int index;
+
+    public TweetPost(int tweetId, int time, int userId, int index) {
+        this.tweetId = tweetId;
+        this.time = time;
+        this.userId = userId;
+        this.index = index;
+    }
+}
