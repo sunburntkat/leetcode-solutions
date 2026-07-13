85a7a87007680ba74dd4bdad83f6e66ca63ce85f
sunburntkat <kathirwork13@gmail.com>
2026-07-13 09:07:46 +0530

    0421-maximum-xor-of-two-numbers-in-an-array Medium 07-13-2026

--- LeetCode/Medium/0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.java
+++ LeetCode/Medium/0421-maximum-xor-of-two-numbers-in-an-array/0421-maximum-xor-of-two-numbers-in-an-array.java
@@ -1,77 +1,29 @@
 class Solution {
     public int findMaximumXOR(int[] nums) {
-        int max = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > max) {
-                max = nums[i];
+        int max=0;
+        for(int num:nums){
+            if(num>max){
+                max=num;
             }
         }
-        int maxBitLength = Integer.SIZE - Integer.numberOfLeadingZeros(max);
-        TrieNode trie = buildTrie(nums, maxBitLength);
-        int maxXOR = 0;
-        for (int i = 1; i < nums.length; i++) {
-            TrieNode curr = trie;
-            int bitLength = Integer.SIZE - Integer.numberOfLeadingZeros(nums[i]);
-            int extraBitLength = maxBitLength - bitLength;
-            int XOR = 0;
-            for (int j = 0; j < extraBitLength; j++) {
-                if (curr.children[1] != null) {
-                    XOR = XOR * 2 + 1;
-                    curr = curr.children[1];
-                } else {
-                    XOR*=2;
-                    curr = curr.children[0];
+        
+        int mask=Integer.highestOneBit(max);
+        int result=0, prefixMask=0;
+        while(mask>0){
+            prefixMask|=mask;
+            int candidate=result|mask;
+            HashSet<Integer> set=new HashSet<>();
+            for(int num: nums){
+                int prefix=prefixMask & num;
+                set.add(prefix);
+                if(set.contains(prefix^candidate)){
+                    result=candidate;
+                    break;
                 }
             }
-            for (int mask = Integer.highestOneBit(nums[i]); mask > 0; mask >>= 1) {
-                int digit = ((nums[i] & mask) != 0) ? 1 : 0;
-                if (curr.children[digit == 0 ? 1 : 0] != null) {
-                    XOR = XOR * 2 + 1;
-                    curr = curr.children[digit == 0 ? 1 : 0];
-                } else {
-                    XOR *= 2;
-                    curr = curr.children[digit];
-                }
-            }
-
-            if (XOR > maxXOR) {
-                maxXOR = XOR;
-            }
+            mask>>=1;
         }
-        return maxXOR;
-    }
-
-    public TrieNode buildTrie(int[] nums, int maxBitLength) {
-        TrieNode root = new TrieNode();
-        for (int i = 0; i < nums.length; i++) {
-            TrieNode curr = root;
-            int bitLength = Integer.SIZE - Integer.numberOfLeadingZeros(nums[i]);
-            int extraBitLength = maxBitLength - bitLength;
-            for (int j = 0; j < extraBitLength; j++) {
-                if (curr.children[0] == null) {
-                    curr.children[0] = new TrieNode();
-                }
-                curr = curr.children[0];
-            }
-            for (int mask = Integer.highestOneBit(nums[i]); mask > 0; mask >>= 1) {
-                int digit = ((nums[i] & mask) != 0) ? 1 : 0;
-                if (curr.children[digit] == null) {
-                    curr.children[digit] = new TrieNode();
-                }
-                curr = curr.children[digit];
-            }
-
-            curr.endWord = true;
-        }
-        return root;
+        return result;
     }
 }
 
-class TrieNode {
-    public boolean endWord;
-    public TrieNode[] children;
-
-    public TrieNode() {
-        children = new TrieNode[2];
-    }
-}
