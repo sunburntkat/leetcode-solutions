8d02a4aa67abb85715fa33533ae5ea4671b0992d
sunburntkat <kathirwork13@gmail.com>
2026-07-13 21:36:50 +0530

    0827-making-a-large-island Hard 07-13-2026

--- LeetCode/Hard/0827-making-a-large-island/0827-making-a-large-island.java
+++ LeetCode/Hard/0827-making-a-large-island/0827-making-a-large-island.java
@@ -1,65 +1,20 @@
 class Solution {
-    class DSU {
-        int[] parents;
-        public int[] sizes;
-
-        public DSU(int n) {
-            parents = new int[n];
-            sizes = new int[n];
-            for (int i = 0; i < n; i++) {
-                parents[i] = i;
-                sizes[i] = 1;
-            }
-        }
-
-        public int findParent(int node) {
-            if (parents[node] == node) {
-                return node;
-            }
-
-            return parents[node] = findParent(parents[node]);
-        }
-
-        public void union(int n1, int n2) {
-            int p1 = findParent(n1);
-            int p2 = findParent(n2);
-
-            if (p1 == p2)
-                return;
-
-            if (sizes[p1] > sizes[p2]) {
-                parents[p2] = p1;
-                sizes[p1] += sizes[p2];
-            } else {
-                parents[p1] = p2;
-                sizes[p2] += sizes[p1];
-            }
-        }
-
-    }
+    int[] dx = { 0, 0, -1, 1 };
+    int[] dy = { -1, 1, 0, 0 };
 
     public int largestIsland(int[][] grid) {
         int n = grid.length;
 
-        DSU ds = new DSU(n * n);
-        int[] dx = { 0, 0, -1, 1 };
-        int[] dy = { -1, 1, 0, 0 };
+        int[][] groups = new int[n][n];
+        HashMap<Integer, Integer> sizes = new HashMap<>();
+
+        int groupNo = 1;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (grid[i][j] == 0)
+                if (grid[i][j] == 0 || groups[i][j] != 0)
                     continue;
-                for (int k = 0; k < 4; k++) {
-                    int neiY = i + dy[k];
-                    int neiX = j + dx[k];
-                    if (neiX < 0 || neiX >= n || neiY < 0 || neiY >= n) {
-                        continue;
-                    }
-                    int neighbour = grid[neiY][neiX];
-                    if (neighbour == 0)
-                        continue;
-                    int neighbourNode = (neiY) * n + (neiX);
-                    ds.union(i * n + j, neighbourNode);
-                }
+                groupNo++;
+                bfs(grid, groups, sizes, n, j, i, groupNo);
             }
         }
         int maxSize = 0;
@@ -67,6 +22,7 @@ class Solution {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] != 0)
                     continue;
+
                 int size = 1;
                 HashSet<Integer> visited = new HashSet<>();
                 for (int k = 0; k < 4; k++) {
@@ -75,26 +31,53 @@ class Solution {
                     if (neiX < 0 || neiX >= n || neiY < 0 || neiY >= n) {
                         continue;
                     }
-                    int neighbour = grid[neiY][neiX];
-                    if (neighbour == 0)
+                    if (grid[neiY][neiX] == 0)
                         continue;
-                    int neighbourNode = (neiY) * n + (neiX);
-                    int parent = ds.findParent(neighbourNode);
-                    if (visited.contains(parent)) {
+                    groupNo = groups[neiY][neiX];
+                    if (visited.contains(groupNo)) {
                         continue;
                     }
-                    visited.add(parent);
-                    size += ds.sizes[parent];
+                    visited.add(groupNo);
+                    size += sizes.get(groupNo);
                 }
                 maxSize = Math.max(maxSize, size);
             }
         }
         if (maxSize != 0)
             return maxSize;
+        
         int maxSingle = 0;
-        for (int i = 0; i < n*n; i++) {
-            maxSingle = Math.max(maxSingle, ds.sizes[i]);
+        for (int size:sizes.values()) {
+            maxSingle = Math.max(maxSingle, size);
         }
-        return maxSingle < n * n ? maxSingle + 1 : maxSingle;
+        return maxSingle;
+    }
+
+    public void bfs(int[][] grid, int[][] groups, HashMap<Integer, Integer> sizes, int n, int x, int y, int groupNo) {
+        Queue<int[]> q = new LinkedList<>();
+
+        int size = 0;
+        q.offer(new int[] { x, y });
+        while (!q.isEmpty()) {
+            int[] node = q.poll();
+            int X = node[0];
+            int Y = node[1];
+            if (groups[Y][X] != 0)
+                continue;
+            groups[Y][X] = groupNo;
+            size++;
+            for (int k = 0; k < 4; k++) {
+                int neiY = Y + dy[k];
+                int neiX = X + dx[k];
+                if (neiX < 0 || neiX >= n || neiY < 0 || neiY >= n) {
+                    continue;
+                }
+                if (grid[neiY][neiX] == 0 || groups[neiY][neiX] != 0)
+                    continue;
+                q.offer(new int[] { neiX, neiY });
+            }
+        }
+        sizes.put(groupNo, size);
+
     }
 }
