b69bcb7e3adf1011c1be977e2f9908418d4bc65f
sunburntkat <kathirwork13@gmail.com>
2026-07-13 22:31:54 +0530

    0846-hand-of-straights Medium 07-13-2026

--- LeetCode/Medium/0846-hand-of-straights/0846-hand-of-straights.java
+++ LeetCode/Medium/0846-hand-of-straights/0846-hand-of-straights.java
@@ -1,30 +1,33 @@
 class Solution {
     public boolean isNStraightHand(int[] hand, int groupSize) {
-        if (hand.length % groupSize!=0) return false;
-        PriorityQueue<Integer> pq=new PriorityQueue<>();
-        for(int i=0;i<hand.length;i++){
-            pq.add(hand[i]);
+        if (hand.length % groupSize != 0)
+            return false;
+        HashMap<Integer, Integer> freq = new HashMap<>();
+        for (int card : hand) {
+            freq.put(card, freq.getOrDefault(card,0) + 1);
         }
-        Queue<Integer> q=new LinkedList<>();
-        while(!pq.isEmpty()){
-            int i=0;
-            int num=pq.peek()-1;
-            while(!pq.isEmpty() && i<groupSize){
-                int next=pq.poll();
-                if(next>num+1){
+        int[][] freqArray = new int[freq.size()][];
+        int i = 0;
+        for (int key : freq.keySet()) {
+            freqArray[i++] = new int[] { key, freq.get(key) };
+        }
+        Arrays.sort(freqArray, (a, b) -> Integer.compare(a[0], b[0]));
+        int p = 0;
+        int n = hand.length;
+        for (int j = 0; j < n; j += groupSize) {
+            int l = p;
+            freqArray[p][1]--;
+            p++;
+            for (int k = 1; k < groupSize; k++) {
+                if(p>=freqArray.length || freqArray[p][1] == 0 ||freqArray[p][0]!=freqArray[p-1][0]+1) {
                     return false;
                 }
-                if(next==num){
-                    q.offer(next);
-                }
-                else{
-                    num=next;
-                    i++;
-                }
+                freqArray[p][1]--;
+                p++;
             }
-            if(!q.isEmpty() && pq.isEmpty()) return false;
-            while(!q.isEmpty()){
-                pq.add(q.poll());
+            p=l;
+            while (p<freqArray.length && freqArray[p][1] == 0) {
+                p++;
             }
         }
         return true;
