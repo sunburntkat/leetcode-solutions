a5c0a17092c4e6f5f701e891644002c5e34d12d0
sunburntkat <kathirwork13@gmail.com>
2026-07-18 12:59:32 +0530

    0155-min-stack Medium 07-18-2026

---
+++ LeetCode/Medium/0155-min-stack/0155-min-stack.java
@@ -0,0 +1,46 @@
+class MinStack {
+    Stack<Node> s = new Stack<>();
+
+    public MinStack() {
+
+    }
+
+    public void push(int value) {
+        if (s.isEmpty()) {
+            s.push(new Node(value, value));
+        } else {
+            s.push(new Node(value, Math.min(value, getMin())));
+        }
+    }
+
+    public void pop() {
+        s.pop();
+    }
+
+    public int top() {
+        return s.peek().val;
+    }
+
+    public int getMin() {
+        return s.peek().min;
+    }
+}
+
+class Node {
+    public int val;
+    public int min;
+
+    public Node(int val, int min) {
+        this.val = val;
+        this.min = min;
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(value);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
