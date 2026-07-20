6d2ec09041c4d6fc3e9641e7cbaca87f180e4ec9
sunburntkat <kathirwork13@gmail.com>
2026-07-20 13:34:22 +0530

    0516-longest-palindromic-subsequence Medium 07-20-2026

---
+++ LeetCode/Medium/0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int longestPalindromeSubseq(String s) {
+        int n=s.length();
+        int[][] dp=new int[n][n];
+
+        for(int i=0;i<n;i++){
+            dp[i][i]=1;
+            for(int j=i-1;j>=0;j--){
+                if(s.charAt(i)==s.charAt(j)){
+                    dp[i][j]=dp[i-1][j+1]+2;
+                }
+                else{
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j+1]);
+                }
+            }
+        }
+        return dp[n-1][0];
+    }
+}
