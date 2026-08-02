d0f1e71450a4f55fd18994012141bcef9fa45ddc
sunburntkat <kathirwork13@gmail.com>
2026-08-02 18:04:38 +0530

    1934-confirmation-rate Medium 08-02-2026

---
+++ LeetCode/Medium/1934-confirmation-rate/1934-confirmation-rate.sql
@@ -0,0 +1,15 @@
+/* Write your T-SQL query statement below */
+SELECT s.user_id, 
+    ROUND(SUM(
+        CASE 
+        WHEN c.action='confirmed'
+        THEN 
+            1.0
+        ELSE
+            0
+        END
+    )/COUNT(*),2) as confirmation_rate
+FROM Signups s
+LEFT JOIN Confirmations c 
+ON s.user_id=c.user_id
+GROUP BY s.user_id
