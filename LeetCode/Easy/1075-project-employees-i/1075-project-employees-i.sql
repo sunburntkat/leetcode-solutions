3a40c31c6345b7647545ea3f191f8c3b2a5bb5c2
sunburntkat <kathirwork13@gmail.com>
2026-08-02 18:47:34 +0530

    1075-project-employees-i Easy 08-02-2026

---
+++ LeetCode/Easy/1075-project-employees-i/1075-project-employees-i.sql
@@ -0,0 +1,7 @@
+/* Write your T-SQL query statement below */
+SELECT p.project_id, ROUND(AVG(CONVERT(DECIMAL,e.experience_years)),2) as average_years
+FROM Project p
+JOIN Employee e
+ON p.employee_id=e.employee_id
+GROUP BY p.project_id
+ORDER BY p.project_id;
