77f6a2a85716a740d1b9434a67ad16074faa0b89
sunburntkat <kathirwork13@gmail.com>
2026-08-02 17:05:46 +0530

    1280-students-and-examinations Easy 08-02-2026

--- LeetCode/Easy/1280-students-and-examinations/1280-students-and-examinations.sql
+++ LeetCode/Easy/1280-students-and-examinations/1280-students-and-examinations.sql
@@ -1,12 +1,9 @@
 /* Write your T-SQL query statement below */
-WITH exam_attendance AS (
-    SELECT student_id, subject_name, COUNT(*) as attended_exams
-    FROM Examinations
-    GROUP BY student_id, subject_name
-)
-SELECT s.student_id, s.student_name, sub.subject_name, ISNULL(e.attended_exams,0) as attended_exams 
+
+SELECT s.student_id, s.student_name, sub.subject_name, COUNT(e.subject_name) AS attended_exams 
 FROM Students s
 CROSS JOIN Subjects sub
-LEFT JOIN exam_attendance e
+LEFT JOIN Examinations e
 ON s.student_id=e.student_id AND e.subject_name=sub.subject_name
-ORDER BY student_id, subject_name
+GROUP BY s.student_id,s.student_name, sub.subject_name
+ORDER BY s.student_id, sub.subject_name
